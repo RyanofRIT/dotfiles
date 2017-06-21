2f7fd303edcb492bad321637eb543e5bd0da02df
Ryan Bell <rjb1928@rit.edu>
2017-06-21 15:24:07 +0100

    wrapped weather script to prevent traceback from being printed to the screen

--- polybar/.config/polybar/weather.py
+++ polybar/.config/polybar/weather.py
@@ -7,8 +7,10 @@ api_key = "480f6e5190ddcc81f415d3ce8c3da384"
 units = "Imperial"
 unit_key = "F"
 
-weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, api_key, units)).read())[2:-1])
-info = weather["weather"][0]["description"].capitalize()
-temp = int(float(weather["main"]["temp"]))
-
-print("%s, %i °%s" % (info, temp, unit_key))
+try:
+    weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, api_key, units)).read())[2:-1])
+    info = weather["weather"][0]["description"].capitalize()
+    temp = int(float(weather["main"]["temp"]))
+    print("%s, %i °%s" % (info, temp, unit_key))
+except:
+    print("")
